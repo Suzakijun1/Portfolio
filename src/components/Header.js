bcf1710e1e6c57d7be36be09937132e33182d368
suzakijun1 <dyong1997@gmail.com>
2023-05-27 14:14:39 -0400

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+function Header() {
+  return <header style={styles.header} className="Header"></header>;
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container-fluid">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              Home
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
