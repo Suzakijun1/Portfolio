fc7848396dfba8939cdc65a2c63b7b602eb93953
suzakijun1 <dyong1997@gmail.com>
2023-05-31 12:11:32 -0400

    finished footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { FooterContainer } from "./Footer.styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
-    <nav className="navbar fixed-bottom bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <FontAwesomeIcon icon={faGithub} beat />
-          Fixed bottom
+    <nav className="navbar fixed-bottom d-flex justify-content-center">
+      <FooterContainer className="container-fluid justify-content-center">
+        <a className="navbar-brand background-white" href="#">
+          <FontAwesomeIcon icon={faGithub} size="lg" />
+          Github
         </a>
-      </div>
+        <a
+          className="navbar-brand"
+          href="https://www.linkedin.com/in/dylan-yong-96830524a/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="lg" />
+          Linkedin
+        </a>
+      </FooterContainer>
     </nav>
   );
 }
--- src/components/Footer/Footer.styles.js
+++ src/components/Footer/Footer.styles.js
@@ -1,1 +1,17 @@
 import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  /* background-color: grey; */
+  color: white;
+  .navbar-brand {
+    margin-right: 2rem;
+    margin-left: 2rem;
+    color: white;
+    font-size: 1.5rem;
+    font-weight: bold;
+    &:hover {
+      box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+      transform: scale(1.1);
+    }
+  }
+`;
