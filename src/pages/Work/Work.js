3b1c6db0c48f65790a1c8ff3176752209fb51d69
suzakijun1 <dyong1997@gmail.com>
2023-07-27 17:24:29 -0400

    added crow to the project

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -3,6 +3,7 @@ import { WorkSec } from "./Work.styles";
 import BiteBud from "../../Assets/biteBud.png";
 import MentalOasis from "../../Assets/mental-oasis-picture.png";
 import heroShowdown from "../../Assets/hero-showdown-pic.jpg";
+import CrowPicture from "../../Assets/Crow_picture.png";
 import { MyDropdown, MyDropdown2, MyDropdown3 } from "./Dropdown";
 import Zoom from "react-reveal/Zoom";
 export default function Work() {
@@ -18,10 +19,19 @@ export default function Work() {
 
   const projects = [
     {
+      title: "Crow",
+      image: CrowPicture,
+      description:
+        "Crow is a desktop application that provides users with a customizable dashboard for storing macros and functions.",
+      projectLink: "",
+      githubLink: "https://github.com/Suzakijun1/Desktop-Dashboard",
+      technologyUsed: "React, Electron.js, Node.js",
+    },
+    {
       title: "Superhero Showdown",
       image: heroShowdown,
       description:
-        "Superhero Showdown is an interactive web game that combines the powers of React, Node.js, Express.js, MongoDB, GraphQL, Apollo, and Heroku. With two exciting game modes, 'Higher Lower' and 'Draft Game', players can immerse themselves in the world of superheroes. In 'Draft Game', strategic team building and turn-based battles await, while 'Higher Lower' challenges players to compare and choose attributes wisely. Game stats, powered by MongoDB and GraphQL, are securely stored and accessible in user profiles. With a responsive and engaging user interface built using React, Superhero Showdown delivers an exhilarating gaming experience for superhero enthusiasts.",
+        "Superhero Showdown is an interactive web game with two exciting modes, 'Higher Lower' and 'Draft Game', delivering an exhilarating superhero experience with strategic battles and a responsive interface.",
       projectLink: "https://hero-showdown.herokuapp.com/",
       githubLink: "https://github.com/Suzakijun1/Higher-Lower-Game",
       technologyUsed:
@@ -33,7 +43,7 @@ export default function Work() {
       title: "Mental Oasis",
       image: MentalOasis,
       description:
-        "Mental Oasis showcases a comprehensive mental health tracker that empowers users to take control of their well-being. With features such as medication intake tracking, inspirational quotes, a customizable calendar, journal entries, and links to helpline websites, the project aims to reduce the negative impact of mental health challenges. By combining these elements, the mental health tracker offers users a holistic tool to actively monitor and improve their mental well-being.",
+        "Mental Oasis is a mental health tracker with medication intake, inspirational quotes, customizable calendar, journal entries, and helpline links for holistic well-being improvement. ",
       projectLink: "#",
       githubLink: "https://github.com/DavidPeguero/Mental-Oasis",
       technologyUsed: "HTML, CSS, JavaScript, Node.js, Express.js, mySQL",
@@ -43,7 +53,7 @@ export default function Work() {
       title: "BiteBud (Requires New API Key)",
       image: BiteBud,
       description:
-        "Bite-Bud is an innovative web application designed to enhance users' exploration of cities and states. By leveraging data from third-party APIs, this application enables users to effortlessly search and discover local restaurants and attractions in their desired locations. With real-time information, Bite-Bud empowers users to make informed decisions and embark on memorable experiences during their travels.",
+        "Bite-Bud is an innovative web application designed to enhance users' exploration of cities and states. By leveraging data from third-party APIs, this application enables users to effortlessly search and discover local restaurants and attractions in their desired locations.",
       projectLink: "https://suzakijun1.github.io/BiteBud/",
       githubLink: "https://github.com/Suzakijun1/BiteBud",
       technologyUsed: "HTML, CSS, JavaScript, Two Server-side APIs",
